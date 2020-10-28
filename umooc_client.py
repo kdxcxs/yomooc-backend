fd659e666eb4bc76cafb1b6ffcc89756b668ad69
kdxcxs <cx@kdxcxs.com>
2020-10-28 19:21:56 +0800

    feat(umooc_simulator): support getting single page of topic list

--- umooc_client.py
+++ umooc_client.py
@@ -4,6 +4,7 @@ A client that simulates the desktop device to communicate with umooc server
 
 import requests
 import time
+import re
 
 
 class LoginError(BaseException):
@@ -17,7 +18,8 @@ class LoginError(BaseException):
 
 class UmoocClient(object):
     def __init__(self):
-        self.session = ''
+        self.js_session_id = ''
+        self.dwr_session_id = ''
 
     def login(self, username, password):
         resp = requests.post('http://eol.ctbu.edu.cn/meol/loginCheck.do',
@@ -33,9 +35,60 @@ class UmoocClient(object):
                                   f'&enterLid=46445'
                                   f'&IPT_LOGINUSERNAME={username}'
                                   f'&IPT_LOGINPASSWORD={password}',
-                             allow_redirects=False,
-                             proxies={'http': 'http://127.0.0.1:54385'})
+                             allow_redirects=False)
         if resp.status_code == 302:
-            self.session = resp.cookies['JSESSIONID']
+            self.js_session_id = resp.cookies['JSESSIONID']
         else:
             raise LoginError('Fail to get session')
+
+    def get_topics(self, page):
+        # get dwr session id
+        resp = requests.post('http://eol.ctbu.edu.cn/meol/dwr/call/plaincall/__System.generateId.dwr',
+                             headers={'Origin': 'http://eol.ctbu.edu.cn',
+                                      'Content-Type': 'text/plain',
+                                      'User-Agent': 'yomooc',
+                                      'Referer': 'http://eol.ctbu.edu.cn/meol/jpk/course/layout/newpage/index.jsp'
+                                                 '?courseId=46445',
+                                      'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
+                                      'Cookie': f'JSESSIONID={self.js_session_id}'},
+                             data=f'callCount=1\n'
+                                  f'c0-scriptName=__System\n'
+                                  f'c0-methodName=generateId\n'
+                                  f'c0-id=0\n'
+                                  f'batchId=0\n'
+                                  f'instanceId=0\n'
+                                  f'page=%2Fmeol%2Fjpk%2Fcourse%2Flayout%2Fnewpage%2Findex.jsp%3FcourseId%3D46445\n'
+                                  f'scriptSessionId=\n'
+                                  f'windowName=\n')
+        self.dwr_session_id = re.search(r'[^"]*"\);', resp.text).group()[:-3]
+
+        # get topics
+
+        # it is needed to request some pages before getting the topic list, maybe the server is judging which course the
+        # user is
+        requests.get('http://eol.ctbu.edu.cn/meol/jpk/course/layout/newpage/index.jsp?courseId=46445',
+                     headers={'Upgrade-Insecure-Requests': '1',
+                              'User-Agent': 'yomooc',
+                              'Cookie': f'JSESSIONID={self.js_session_id}; '
+                                        f'DWRSESSIONID={self.dwr_session_id}'})
+        requests.get('http://eol.ctbu.edu.cn/meol/jpk/course/layout/newpage/default_demonstrate.jsp'
+                     '?courseId=46445',
+                     headers={'Upgrade-Insecure-Requests': '1',
+                              'User-Agent': 'yomooc',
+                              'Referer': 'http://eol.ctbu.edu.cn/meol/jpk/course/layout/newpage/index.jsp'
+                                         '?courseId=46445',
+                              'Cookie': f'JSESSIONID={self.js_session_id}; '
+                                        f'DWRSESSIONID={self.dwr_session_id}'})
+        resp = requests.get(f'http://eol.ctbu.edu.cn/meol/common/faq/forum.jsp'
+                            f'?viewtype=thread'
+                            f'&forumid=102211'
+                            f'&cateId=0'
+                            f'&s_gotopage={page}',
+                            headers={'Upgrade-Insecure-Requests': '1',
+                                     'User-Agent': 'yomooc',
+                                     'Referer': 'http://eol.ctbu.edu.cn/meol/common/faq/forum.jsp'
+                                                '?count=MODITIME'
+                                                '&forumid=102211',
+                                     'Cookie': f'JSESSIONID={self.js_session_id}; '
+                                               f'DWRSESSIONID={self.dwr_session_id}'})
+        print(resp.text)
